6ab87b4a74a22286ee6e57fb1513dd884fd4fb13
Fiadhnait <fiadhnait.mcdonough@ark-energy.eu>
2015-07-15 14:12:23 +0100

    CasperJsDemo- Changing path to bootstrap.js

--- CasperJsDemo/Test/CasperJsBin/batchbin/casperjs.bat
+++ CasperJsDemo/Test/CasperJsBin/batchbin/casperjs.bat
@@ -2,4 +2,4 @@
 set CASPER_PATH=%~dp0..
 set CASPER_BIN=%CASPER_PATH%\bin\
 set ARGV=%*
-call phantomjs --ignore-ssl-errors=yes "%CASPER_BIN%bootstrap.js" --casper-path="%CASPER_PATH%" --cli %ARGV%
+call phantomjs --ignore-ssl-errors=yes "\bootstrap.js" --casper-path="%CASPER_PATH%" --cli %ARGV%
