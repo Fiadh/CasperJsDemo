4d64bb8cad694e4d9cb5cb8ebb18282d07990f0f
Fiadhnait <fiadhnait.mcdonough@ark-energy.eu>
2015-07-15 14:34:21 +0100

    CasperJsDemo- Changing casperjs.bat

--- CasperJsDemo/Test/CasperJsBin/batchbin/casperjs.bat
+++ CasperJsDemo/Test/CasperJsBin/batchbin/casperjs.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
-set CASPER_PATH=%~dp0..
+set CASPER_PATH=%~dp0libs\CasperJsBin
 set CASPER_BIN=%CASPER_PATH%\bin\
+set PHANTOMJS=%~dp0libs\PhantomJsBin\phantomjs.exe
 set ARGV=%*
-call phantomjs --ignore-ssl-errors=yes "\bootstrap.js" --casper-path="%CASPER_PATH%" --cli %ARGV%
+
+call "%PHANTOMJS%" "%CASPER_PATH%bootstrap.js" --casper-path="%CASPER_PATH%" --cli %ARGV%
