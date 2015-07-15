7b3ef29bbe6fa97870adda3d4a4179a7e5e7ef80
Fiadhnait <fiadhnait.mcdonough@ark-energy.eu>
2015-07-15 11:55:57 +0100

    CasperJsDemo- Changing path to phantomjs

--- CasperJsDemo/Test/CasperJsBin/batchbin/casperjs.bat
+++ CasperJsDemo/Test/CasperJsBin/batchbin/casperjs.bat
@@ -2,5 +2,4 @@
 set CASPER_PATH=%~dp0..
 set CASPER_BIN=%CASPER_PATH%\bin\
 set ARGV=%*
-echo %ARGV%
-call %~dp0..\..\phantomjs\phantomjs "%CASPER_BIN%bootstrap.js" --casper-path="%CASPER_PATH%" --cli %ARGV%
+call phantomjs "%CASPER_BIN%bootstrap.js" --casper-path="%CASPER_PATH%" --cli %ARGV%
